0549dccaec6a72ebfccd76e1aea7706486b19d43
cmorlok <ChristianMorlok@gmail.com>
2014-11-25 13:31:56 +0000

    Disable power menu on secured lockscreen
    It is a security flaw to allow access to the power menu when a user has explicity locked his/her lockscreen.
    Set default value to false (disabled) and allow changing it with an overlay.
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1056,7 +1056,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void powerLongPress() {
-        final int behavior = getResolvedLongPressOnPowerBehavior();
+        int behavior = getResolvedLongPressOnPowerBehavior();
         switch (behavior) {
         case LONG_PRESS_POWER_NOTHING:
             break;
@@ -1065,7 +1065,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false)) {
                 performAuditoryFeedbackForAccessibilityIfNeed();
             }
-            showGlobalActionsInternal();
+             boolean locked = isStatusBarKeyguard() && isKeyguardSecure();
+            boolean globalActionsOnLockScreen = mContext.getResources().getBoolean(com.android.internal.R.bool.config_powerButtonLockscreenEnabled);
+            if (locked && !globalActionsOnLockScreen) {
+                behavior = LONG_PRESS_POWER_NOTHING;
+            } else {
+                showGlobalActionsInternal();
+            }
             break;
         case LONG_PRESS_POWER_SHUT_OFF:
         case LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM:
