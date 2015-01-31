3ad1da128cda6a11ddf4ef7cf5fcaef0d4a46ef7
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 22:45:50 +0000

    SystemUI: QS: Add Sync Tile (w/ overlay)
    3rd tile without CM's implementation. It's pretty easy after all. Credits to the original author of the commits below and zephiK.
    
    This is a combination of 2 commits.
    
    QS: add Sync tile (1/2)
    
    Change-Id: I0c0bb51e736a01661566f1118f955e32eba96063
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/chroma_strings.xml
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/icarus_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    QS: Sync tile cleanup (1/2)
    
    * (Get inline with cm)
    * Add vector drawables (thanks to cm)
    
    Conflicts:
    	packages/SystemUI/res/drawable-hdpi/ic_qs_sync_off.png
    	packages/SystemUI/res/drawable-hdpi/ic_qs_sync_on.png
    	packages/SystemUI/res/drawable-xhdpi/ic_qs_sync_off.png
    	packages/SystemUI/res/drawable-xhdpi/ic_qs_sync_on.png
    	packages/SystemUI/res/drawable-xxhdpi/ic_qs_sync_off.png
    	packages/SystemUI/res/drawable-xxhdpi/ic_qs_sync_on.png
    	packages/SystemUI/res/values/strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -263,6 +264,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
         else if (tileSpec.equals("timeout")) return new ScreenTimeoutTile(this);
+        else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
