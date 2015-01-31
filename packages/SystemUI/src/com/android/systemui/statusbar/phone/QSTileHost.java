a30c3ff61a4e24736a98184067bd99d4512d9a29
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 21:49:21 +0000

    Add Screenshot QS Tile
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/purenexus_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -297,6 +298,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals(CastTile.SPEC)) return new CastTile(this);
         else if (tileSpec.equals(HotspotTile.SPEC)) return new HotspotTile(this);
         else if (tileSpec.equals(ScreenOffTile.SPEC)) return new ScreenOffTile(this);
+        else if (tileSpec.equals(ScreenshotTile.SPEC)) return new ScreenshotTile(this);
         else if (tileSpec.equals(ScreenTimeoutTile.SPEC)) return new ScreenTimeoutTile(this);
         else if (tileSpec.equals(SoundTile.SPEC)) return new SoundTile(this);
         else if (tileSpec.equals(SyncTile.SPEC)) return new SyncTile(this);
