5d38b0d6a3c50d31b2f6a9e1be5288edc1f34996
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2015-04-07 18:41:51 +0900

    Add Sound tile to Quick Settings
    Add Sound tile to make user easier to change sound mode like the old pre-lollipop way

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -199,6 +199,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("hotspot")) return R.string.quick_settings_hotspot_label;
         else if (spec.equals("screenoff")) return R.string.quick_settings_screen_off;
         else if (spec.equals("timeout")) return R.string.quick_settings_timeout_label;
+        else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
         return 0;
     }
@@ -445,6 +446,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("hotspot")) return R.drawable.ic_hotspot_enable;
             else if (mSpec.equals("screenoff")) return R.drawable.ic_qs_power;
             else if (mSpec.equals("timeout")) return R.drawable.ic_qs_screen_timeout_vector;
+            else if (mSpec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
             else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
             return R.drawable.android;
         }
