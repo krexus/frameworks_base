596984a860febe36a28c99e56f1b67ce72e6e3da
George G <kreach3r@users.noreply.github.com>
2015-05-28 17:14:09 +0300

    FB: Add charging battery light option (2/3)
    10/2015: Ported to Marshmallow
    
    Defaults to false (disabled)
    Requires commit on device/vendor (for the device support)
    Requires commit on Settings
    
    Requires the kernel's light always on patch
    
    Conflicts:
    	core/res/res/values/icarus_config.xml
    	core/res/res/values/icarus_symbols.xml

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2384,6 +2384,9 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
             loadBooleanSetting(stmt, Settings.System.NOTIFICATION_LIGHT_PULSE,
                     R.bool.def_notification_pulse);
+                    
+            loadBooleanSetting(stmt, Settings.System.CHARGING_BATTERY_LED,
+                    R.bool.def_charging_led);                    
 
             loadUISoundEffectsSettings(stmt);
 
