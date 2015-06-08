ffd07f4696a340d3d337ddbd73fa660bd8f3927a
George G <kreach3r@users.noreply.github.com>
2015-06-08 17:55:13 +0300

    Long press "Reboot" to "Reboot to safe mode"
    Moved the longpress Dialog from Power to Reboot.

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -333,16 +333,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         return dialog;
     }
 
-    private final class PowerAction extends SinglePressAction implements LongPressAction {
+    private final class PowerAction extends SinglePressAction {
         private PowerAction() {
             super(com.android.internal.R.drawable.ic_lock_power_off,
                 R.string.global_action_power_off);
-        }
-
-        @Override
-        public boolean onLongPress() {
-            mWindowManagerFuncs.rebootSafeMode(true);
-            return true;
         }
 
         @Override
@@ -362,10 +356,16 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     }
 
-    private final class RebootAction extends SinglePressAction {
+    private final class RebootAction extends SinglePressAction implements LongPressAction {
         private RebootAction() {
             super(com.android.internal.R.drawable.ic_lock_power_reboot,
                     R.string.global_action_reboot);
+        }
+
+        @Override
+        public boolean onLongPress() {
+            mWindowManagerFuncs.rebootSafeMode(true);
+            return true;
         }
 
         @Override
