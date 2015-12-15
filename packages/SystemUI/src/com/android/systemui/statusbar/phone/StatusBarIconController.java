fbe019c2f545d2936aafaa808f17859248c18059
bigrushdog <randall.rushing@gmail.com>
2015-12-15 14:06:14 -0500

    statusbar: fix notification icons bleeding into center clock view
    -- Pass StatusbarIconController to Clock instances. mIconController
       was declared in Clock, but was never set.
    
    -- Include status bar Carrier label in IconMerger
       width calculation
    
    -- Move "more" icon to the right of IconMerger. This is more inline
       with AOSP. Also, it seems we want status bar carrier label
       persistently aligned to the far left of the status bar

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -80,11 +80,11 @@ public class StatusBarIconController implements Tunable {
     private ImageView mMoreIcon;
     private BatteryMeterView mBatteryMeterView;
     private BatteryLevelTextView mBatteryLevelTextView;
-    private TextView mClock;
+    private Clock mClock;
     // Left or center clock
     private LinearLayout mCenterClockLayout;
-    private TextView mCenterClock;
-    private TextView mLeftClock;
+    private Clock mCenterClock;
+    private Clock mLeftClock;
     private boolean mShowClock;
     private int mClockLocation;
 
@@ -131,10 +131,10 @@ public class StatusBarIconController implements Tunable {
         mStatusIconsKeyguard = (LinearLayout) keyguardStatusBar.findViewById(R.id.statusIcons);
         mBatteryMeterView = (BatteryMeterView) statusBar.findViewById(R.id.battery);
 	mBatteryLevelTextView = (BatteryLevelTextView) statusBar.findViewById(R.id.battery_level_text);
-        mClock = (TextView) statusBar.findViewById(R.id.clock);
-        mCenterClock = (TextView) statusBar.findViewById(R.id.center_clock);
-        mLeftClock = (TextView) statusBar.findViewById(R.id.left_clock);
-        mCenterClockLayout = (LinearLayout)statusBar.findViewById(R.id.center_clock_layout); 
+        mClock = (Clock) statusBar.findViewById(R.id.clock);
+        mCenterClock = (Clock) statusBar.findViewById(R.id.center_clock);
+        mLeftClock = (Clock) statusBar.findViewById(R.id.left_clock);
+        mCenterClockLayout = (LinearLayout)statusBar.findViewById(R.id.center_clock_layout);
         mLinearOutSlowIn = AnimationUtils.loadInterpolator(mContext,
                 android.R.interpolator.linear_out_slow_in);
         mFastOutSlowIn = AnimationUtils.loadInterpolator(mContext,
@@ -143,6 +143,10 @@ public class StatusBarIconController implements Tunable {
         mLightModeIconColorSingleTone = context.getColor(R.color.light_mode_icon_color_single_tone);
         mHandler = new Handler();
         updateResources();
+
+        mClock.setIconController(this);
+        mCenterClock.setIconController(this);
+        mLeftClock.setIconController(this);
 
         TunerService.get(mContext).addTunable(this, ICON_BLACKLIST);
     }
