d5f102f5220bec1d57470369214541736a80c22f
camcory <camcory1@gmail.com>
2016-05-16 21:44:07 +0300

    Fix FC on volume key press while on 3dot menu
    This happens on every app.
    We limited it down to the "swap volume keys on rotation" commit
    Logcat: http://hastebin.com/vetosokuga.avrasm
    Related line: android.media.session.MediaSessionLegacyHelper.sendVolumeKeyEvent(MediaSessionLegacyHelper.java:210)

--- core/java/com/android/internal/policy/PhoneWindow.java
+++ core/java/com/android/internal/policy/PhoneWindow.java
@@ -1794,8 +1794,8 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
                 }
                 final int rotation = getWindowManager().getDefaultDisplay().getRotation();
                 final Configuration config = getContext().getResources().getConfiguration();
-                final boolean swapKeys = Settings.System.getInt(getContext().getContentResolver(),
-                        Settings.System.SWAP_VOLUME_BUTTONS, 1) == 1;
+                final boolean swapKeys = Settings.System.getIntForUser(getContext().getContentResolver(),
+                        Settings.System.SWAP_VOLUME_BUTTONS, 1, UserHandle.USER_CURRENT_OR_SELF) == 1;
 
                 if (swapKeys
                         && (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_180)
