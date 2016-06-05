a7d59ac40a67c13aff5405b596b739c1618f56d6
0xD34D <clark@scheffsblend.com>
2016-06-05 04:25:44 +0300

    OMS: Themes should be dynamically theming the power menu
    OMS introduced a different way of loading resources into apps, and this new behavior is still buggy. Power menu wasn't getting themed with
    our working oms code.
    
    This commit:
     - Picks the following commits from CM
     - Introduces a new helper class: ThemeUtils
     - A lot of name refactoring, mainly uiContext --> mUiContext
     - We can't check (yet) for theme change as CMTE, so I had to drop some if (mContext != null) guards. I didn't notice any performance difference.
     - Both stock and themes should work
    
    Reworked for OMS by @KreAch3R and @camcory
    
    This is a combination of the following commits:
    
    Show reboot dialog using themed resources: https://github.com/CyanogenMod/android_frameworks_base/commit/d8e138b06abf4147a37d2c3e8f1e95a7e9b52e5c
    by 0xD34D
    
    Themes: Theme power off and reboot dialog: https://github.com/CyanogenMod/android_frameworks_base/commit/f7fc9ceefa1e9cad133d694d93f43e3abf18406e
    by Altaf-Mahdi
    This patch allows theming of the dialog which
    comes after the power menu
    
    Distilled changes from these:
    https://github.com/CyanogenMod/android_frameworks_base/commit/a4e229c7c09f39ee54df4a150826cab9e435f890
    and
    https://github.com/CyanogenMod/cm_platform_sdk/blame/cm-13.0/sdk/src/java/org/cyanogenmod/internal/util/ThemeUtils.java
    
    Conflicts:
    	services/core/java/com/android/server/power/ShutdownThread.java
    
    Change-Id: Ic82fcadeede4c4352db380b3f31f516c7b10877b
    
    Conflicts:
    	services/core/java/com/android/server/power/ShutdownThread.java

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -50,6 +50,7 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.provider.Settings;
 
+import com.android.internal.util.ThemeUtils;
 import com.android.internal.telephony.ITelephony;
 import com.android.server.pm.PackageManagerService;
 
@@ -135,7 +136,7 @@ public final class ShutdownThread extends Thread {
     public static void shutdown(final Context context, boolean confirm) {
         mReboot = false;
         mRebootSafeMode = false;
-        shutdownInner(context, confirm);
+        shutdownInner(getUiContext(context), confirm);
     }
 
     private static boolean isAdvancedRebootPossible(final Context context) {
@@ -182,26 +183,27 @@ public final class ShutdownThread extends Thread {
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             final boolean advancedReboot = isAdvancedRebootPossible(context);
+            final Context mUiContext = getUiContext(context);
 
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
                 sConfirmDialog = null;
             }
-            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context)
+            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(mUiContext)
                     .setTitle(mRebootSafeMode
                             ? com.android.internal.R.string.reboot_safemode_title
                             : ((showRebootOption && mReboot)
                                     ? com.android.internal.R.string.reboot_title
                                     : com.android.internal.R.string.power_off));
 
-           
+
             if (advancedReboot && mReboot && !mRebootSafeMode) {
                 confirmDialogBuilder
                       .setSingleChoiceItems(com.android.internal.R.array.shutdown_reboot_options,
                               0, null);
             } else {
                 confirmDialogBuilder.setMessage(resourceId);
-            }                
+            }
 
             confirmDialogBuilder.setPositiveButton(com.android.internal.R.string.yes,
                     new DialogInterface.OnClickListener() {
@@ -287,7 +289,7 @@ public final class ShutdownThread extends Thread {
         mRebootSafeMode = false;
         mRebootUpdate = false;
         mRebootReason = reason;
-        shutdownInner(context, confirm);
+        shutdownInner(getUiContext(context), confirm);
     }
 
     /**
@@ -806,5 +808,12 @@ public final class ShutdownThread extends Thread {
         if (!done[0]) {
             Log.w(TAG, "Timed out waiting for uncrypt.");
         }
+    }
+
+    private static Context getUiContext(Context context) {
+        Context mUiContext = null;
+        mUiContext = ThemeUtils.createUiContext(context);
+        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
+        return mUiContext != null ? mUiContext : context;
     }
 }
