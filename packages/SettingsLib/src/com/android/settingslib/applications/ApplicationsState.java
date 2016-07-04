de4e43e66ff51307851097bac3c769fcf0287cd1
George G <kreach3r@users.noreply.github.com>
2016-07-04 06:25:15 +0300

    ApplicationsState: add filter for Substratum overlays [2/2]
    This commit allows the framework to handle the filtering of the
    overlays found for OMS.
    
    Change-Id: I7646115e8f73494d726728fac58cc47aafd69d5d

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -188,9 +188,11 @@ public class ApplicationsState {
         // Only the owner can see all apps.
         mAdminRetrieveFlags = PackageManager.MATCH_ANY_USER |
                 PackageManager.MATCH_DISABLED_COMPONENTS |
-                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS;
+                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS |
+                PackageManager.GET_META_DATA;
         mRetrieveFlags = PackageManager.MATCH_DISABLED_COMPONENTS |
-                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS;
+                PackageManager.MATCH_DISABLED_UNTIL_USED_COMPONENTS |
+                PackageManager.GET_META_DATA;
 
         /**
          * This is a trick to prevent the foreground thread from being delayed.
@@ -1521,6 +1523,19 @@ public class ApplicationsState {
         }
     };
 
+    public static final AppFilter FILTER_SUBSTRATUM = new AppFilter() {
+        @Override
+        public void init() {
+        }
+
+        @Override
+        public boolean filterApp(AppEntry entry) {
+            boolean isSubs;
+            isSubs = !(((entry.info.metaData != null) && (entry.info.metaData.getString("Substratum_Parent") != null)));
+            return isSubs;
+        }
+    };
+
     public static final AppFilter FILTER_WORK = new AppFilter() {
         private int mCurrentUser;
 
@@ -1592,21 +1607,7 @@ public class ApplicationsState {
             return false;
         }
     };
-    public static final AppFilter FILTER_SUBSTRATUM = new AppFilter() {
-        @Override
-        public void init() {
-        }
 
-        @Override
-        public boolean filterApp(AppEntry entry) {
-            if (hasFlag(entry.info.flags, ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) {
-                return true;
-            } else if (!hasFlag(entry.info.flags, ApplicationInfo.FLAG_SYSTEM)) {
-                return true;
-            }
-            return false;
-        }
-    };
     public static final AppFilter FILTER_DISABLED = new AppFilter() {
         @Override
         public void init() {
