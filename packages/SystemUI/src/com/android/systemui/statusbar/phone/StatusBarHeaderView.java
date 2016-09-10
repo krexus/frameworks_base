AOSPA/android_frameworks_base-old

SystemUI: Hide specific views for QS reorder
This commit makes specific views for the battery information (battery
meter view and battery level text view) get hidden, instead of the
container they reside in. <insert buzz word of the day>

Change-Id: I0d8e9b2ed8cb9d54cfedcb9b45fa81c30ae3b230

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -93,6 +93,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private TextView mEmergencyCallsOnly;
     private TextView mBatteryLevel;
     private TextView mAlarmStatus;
+    private BatteryMeterView mBatteryMeterView;
 
     private boolean mShowEmergencyCallsOnly;
     private boolean mAlarmShowing;
@@ -174,6 +175,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mBatteryLevel = (TextView) findViewById(R.id.battery_level);
         mAlarmStatus = (TextView) findViewById(R.id.alarm_status);
         mAlarmStatus.setOnClickListener(this);
+        mBatteryMeterView = (BatteryMeterView) findViewById(R.id.battery);
         mSignalCluster = findViewById(R.id.signal_cluster);
         mSystemIcons = (LinearLayout) findViewById(R.id.system_icons);
         loadDimens();
@@ -347,7 +349,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     public void setBatteryController(BatteryController batteryController) {
         mBatteryController = batteryController;
-        ((BatteryMeterView) findViewById(R.id.battery)).setBatteryController(batteryController);
+        mBatteryMeterView.setBatteryController(batteryController);
     }
 
     public void setNextAlarmController(NextAlarmController nextAlarmController) {
@@ -988,7 +990,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             transition(mSettingsContainer, !isInReorderMode);
             transition(mEmergencyCallsOnly, !isInReorderMode && mShowEmergencyCallsOnly);
             transition(mQsAddButton, !isInReorderMode && mQsAbleToShowHidden);
-            transition(mSystemIconsSuperContainer, !isInReorderMode);
+            transition(mBatteryMeterView, !isInReorderMode);
+            transition(mBatteryLevel, !isInReorderMode);
             transition(mQsDeleteHeader, isInReorderMode);
             if (isInReorderMode) {
                 mQsDeleteHeader.getDrawable().setAlpha(0xFF);

krexus/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -99,6 +99,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private BatteryMeterView mBatteryMeter;
     private BatteryLevelTextView mBatteryLevel;
     private TextView mAlarmStatus;
+    private BatteryMeterView mBatteryMeterView;
 
     private boolean mShowEmergencyCallsOnly;
     private boolean mAlarmShowing;
@@ -187,6 +188,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         mBatteryLevel = (BatteryLevelTextView) findViewById(R.id.battery_level_text);
         mAlarmStatus = (TextView) findViewById(R.id.alarm_status);
         mAlarmStatus.setOnClickListener(this);
+        mBatteryMeterView = (BatteryMeterView) findViewById(R.id.battery);
         mSignalCluster = findViewById(R.id.signal_cluster);
         mSystemIcons = (LinearLayout) findViewById(R.id.system_icons);
         mSettingsObserver = new SettingsObserver(new Handler());
@@ -1033,7 +1035,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             transition(mSettingsContainer, !isInReorderMode);
             transition(mEmergencyCallsOnly, !isInReorderMode && mShowEmergencyCallsOnly);
             transition(mQsAddButton, !isInReorderMode && mQsAbleToShowHidden);
-            transition(mSystemIconsSuperContainer, !isInReorderMode);
+            transition(mBatteryMeter, !isInReorderMode);
+            transition(mBatteryLevel, !isInReorderMode);
             transition(mQsDeleteHeader, isInReorderMode);
             if (isInReorderMode) {
                 mQsDeleteHeader.getDrawable().setAlpha(0xFF);