AOSPA/android_frameworks_base-old

SystemUI: Allow QS tile reordering
This commit makes tiles get lifted up upon a long-press. They can then
be dragged around while other tiles move out of the way, retaining
their relative positions. This provides a fairly smooth an natural
reordering experience.

Change-Id: If846b5a8920702b936c405478d8b873379528f2e

packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -57,6 +57,7 @@ public class QSTileView extends ViewGroup {
     private final int mTilePaddingBelowIconPx;
     private final int mDualTileVerticalPaddingPx;
     private final View mTopBackgroundView;
+    private final Drawable mTileBackground;
 
     private TextView mLabel;
     private QSDualTileLabel mDualLabel;
@@ -64,8 +65,8 @@ public class QSTileView extends ViewGroup {
     private OnClickListener mClickPrimary;
     private OnClickListener mClickSecondary;
     private OnLongClickListener mLongClick;
-    private Drawable mTileBackground;
     private RippleDrawable mRipple;
+    private float mRealElevation;
 
     public QSTileView(Context context) {
         super(context);
@@ -119,87 +120,118 @@ public class QSTileView extends ViewGroup {
         }
     }
 
-    private void recreateLabel() {
-        CharSequence labelText = null;
-        CharSequence labelDescription = null;
-        if (mLabel != null) {
-            labelText = mLabel.getText();
-            removeView(mLabel);
-            mLabel = null;
-        }
+    private synchronized void recreateLabel() {
+        final CharSequence labelText;
+        final CharSequence labelDescription;
         if (mDualLabel != null) {
             labelText = mDualLabel.getText();
+            labelDescription = mDualLabel.getContentDescription();
+        } else if (mLabel != null) {
+            labelText = mLabel.getText();
             labelDescription = mLabel.getContentDescription();
-            removeView(mDualLabel);
-            mDualLabel = null;
+        } else {
+            labelText = "";
+            labelDescription = "";
         }
+
         final Resources res = mContext.getResources();
         if (mDual) {
-            mDualLabel = new QSDualTileLabel(mContext);
-            mDualLabel.setId(android.R.id.title);
-            mDualLabel.setBackgroundResource(R.drawable.btn_borderless_rect);
-            mDualLabel.setFirstLineCaret(res.getDrawable(R.drawable.qs_dual_tile_caret));
-            mDualLabel.setTextColor(res.getColor(R.color.qs_tile_text));
-            mDualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
-            mDualLabel.setTypeface(CONDENSED);
-            mDualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+            if (mLabel != null) {
+                removeView(mLabel);
+                mLabel = null;
+            }
+
+            final QSDualTileLabel dualLabel = mDualLabel == null ?
+                    new QSDualTileLabel(mContext) : mDualLabel;
+            dualLabel.setId(android.R.id.title);
+            dualLabel.setBackgroundResource(R.drawable.btn_borderless_rect);
+            dualLabel.setFirstLineCaret(res.getDrawable(R.drawable.qs_dual_tile_caret));
+            dualLabel.setTextColor(res.getColor(R.color.qs_tile_text));
+            dualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
+            dualLabel.setTypeface(CONDENSED);
+            dualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
-            mDualLabel.setClickable(true);
-            mDualLabel.setOnClickListener(mClickSecondary);
-            mDualLabel.setFocusable(true);
-            if (labelText != null) {
-                mDualLabel.setText(labelText);
+            dualLabel.setClickable(true);
+            dualLabel.setOnClickListener(mClickSecondary);
+            dualLabel.setLongClickable(true);
+            dualLabel.setOnLongClickListener(mLongClick);
+            dualLabel.setFocusable(true);
+            dualLabel.setText(labelText);
+            dualLabel.setContentDescription(labelDescription);
+            if (mDualLabel == null) {
+                addView(mDualLabel = dualLabel);
             }
-            if (labelDescription != null) {
-                mDualLabel.setContentDescription(labelDescription);
-            }
-            addView(mDualLabel);
         } else {
-            mLabel = new TextView(mContext);
-            mLabel.setId(android.R.id.title);
-            mLabel.setTextColor(res.getColor(R.color.qs_tile_text));
-            mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
-            mLabel.setMinLines(2);
-            mLabel.setPadding(0, 0, 0, 0);
-            mLabel.setTypeface(CONDENSED);
-            mLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
-            mLabel.setClickable(false);
-            if (labelText != null) {
-                mLabel.setText(labelText);
+            if (mDualLabel != null) {
+                mDualLabel.setOnClickListener(null);
+                mDualLabel.setClickable(false);
+                mDualLabel.setOnLongClickListener(null);
+                mDualLabel.setLongClickable(false);
+                mDualLabel.setFocusable(false);
+
+                removeView(mDualLabel);
+                mDualLabel = null;
             }
-            addView(mLabel);
+
+            final TextView label = mLabel == null ? new TextView(mContext) : mLabel;
+            label.setId(android.R.id.title);
+            label.setTextColor(res.getColor(R.color.qs_tile_text));
+            label.setGravity(Gravity.CENTER_HORIZONTAL);
+            label.setMinLines(2);
+            label.setPadding(0, 0, 0, 0);
+            label.setTypeface(CONDENSED);
+            label.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                    res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
+            label.setClickable(false);
+            label.setText(labelText);
+            label.setContentDescription(labelDescription);
+            if (mLabel == null) {
+                addView(mLabel = label);
+            }
         }
     }
 
-    public boolean setDual(boolean dual) {
+    public boolean isDual() {
+        return mDual;
+    }
+
+    public synchronized void setDual(final boolean dual) {
         final boolean changed = dual != mDual;
         mDual = dual;
-        if (changed) {
-            recreateLabel();
-        }
+
         if (mTileBackground instanceof RippleDrawable) {
             setRipple((RippleDrawable) mTileBackground);
         }
+
+        final View priority = dual ? mTopBackgroundView : this;
+        final View other = dual ? this : mTopBackgroundView;
+
+        priority.setOnClickListener(mClickPrimary);
+        priority.setClickable(true);
+        priority.setOnLongClickListener(mLongClick);
+        priority.setLongClickable(true);
+        other.setOnClickListener(null);
+        other.setClickable(false);
+        other.setOnLongClickListener(null);
+        other.setLongClickable(false);
+
+        setImportantForAccessibility(dual ? View.IMPORTANT_FOR_ACCESSIBILITY_NO :
+                View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+
         if (dual) {
-            mTopBackgroundView.setOnClickListener(mClickPrimary);
-            setOnClickListener(null);
-            setClickable(false);
-            setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
             mTopBackgroundView.setBackground(mTileBackground);
         } else {
-            mTopBackgroundView.setOnClickListener(null);
-            mTopBackgroundView.setClickable(false);
-            setOnClickListener(mClickPrimary);
-            setOnLongClickListener(mLongClick);
-            setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
             setBackground(mTileBackground);
         }
+
         mTopBackgroundView.setFocusable(dual);
         setFocusable(!dual);
         mDivider.setVisibility(dual ? VISIBLE : GONE);
+        if (changed) {
+            recreateLabel();
+            updateTopPadding();
+        }
         postInvalidate();
-        return changed;
     }
 
     private void setRipple(RippleDrawable tileBackground) {
@@ -210,10 +242,11 @@ public class QSTileView extends ViewGroup {
     }
 
     public void init(OnClickListener clickPrimary, OnClickListener clickSecondary,
-            OnLongClickListener longClick) {
+            OnLongClickListener longClick, OnDragListener drag) {
         mClickPrimary = clickPrimary;
         mClickSecondary = clickSecondary;
         mLongClick = longClick;
+        setOnDragListener(drag);
     }
 
     protected View createIcon() {
@@ -324,6 +357,10 @@ public class QSTileView extends ViewGroup {
 
     public void onStateChanged(QSTile.State state) {
         mHandler.obtainMessage(H.STATE_CHANGED, state).sendToTarget();
+    }
+
+    public View.DragShadowBuilder getDragShadowBuilder() {
+        return new View.DragShadowBuilder(mIcon);
     }
 
     private class H extends Handler {

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -933,16 +933,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mUserSwitcherController, mKeyguardMonitor,
                     mSecurityController);
             mQSPanel.setHost(qsh);
-            mQSPanel.setTiles(qsh.getTiles());
-            mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindowContent);
-            mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+            mQSPanel.setBrightnessMirror(mBrightnessMirrorController =
+                    new BrightnessMirrorController(mStatusBarWindowContent));
             mHeader.setQSPanel(mQSPanel);
-            qsh.setCallback(new QSTileHost.Callback() {
-                @Override
-                public void onTilesChanged() {
-                    mQSPanel.setTiles(qsh.getTiles());
-                }
-            });
         }
 
         // User info. Trigger first load.

krexus/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
	packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/AirplaneModeTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/ColorInversionTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/HotspotTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
	packages/SystemUI/src/com/android/systemui/qs/tiles/RotationLockTile.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -45,6 +45,8 @@ import java.util.Objects;
 
 /** View that represents a standard quick settings tile. **/
 public class QSTileView extends ViewGroup {
+    private static final Typeface CONDENSED = Typeface.create("sans-serif-condensed",
+            Typeface.NORMAL);
 
     protected final Context mContext;
     private final View mIcon;
@@ -56,15 +58,16 @@ public class QSTileView extends ViewGroup {
     private final int mTilePaddingBelowIconPx;
     private final int mDualTileVerticalPaddingPx;
     private final View mTopBackgroundView;
+    private final Drawable mTileBackground;
 
     private TextView mLabel;
     private QSDualTileLabel mDualLabel;
     private boolean mDual;
     private OnClickListener mClickPrimary;
     private OnClickListener mClickSecondary;
-    private OnLongClickListener mClickLong;
-    private Drawable mTileBackground;
+    private OnLongClickListener mLongClick;
     private RippleDrawable mRipple;
+    private float mRealElevation;
 
     public QSTileView(Context context) {
         super(context);
@@ -119,88 +122,118 @@ public class QSTileView extends ViewGroup {
         }
     }
 
-    private void recreateLabel() {
-        CharSequence labelText = null;
-        CharSequence labelDescription = null;
-        if (mLabel != null) {
-            labelText = mLabel.getText();
-            removeView(mLabel);
-            mLabel = null;
-        }
+    private synchronized void recreateLabel() {
+        final CharSequence labelText;
+        final CharSequence labelDescription;
         if (mDualLabel != null) {
             labelText = mDualLabel.getText();
+            labelDescription = mDualLabel.getContentDescription();
+        } else if (mLabel != null) {
+            labelText = mLabel.getText();
             labelDescription = mLabel.getContentDescription();
-            removeView(mDualLabel);
-            mDualLabel = null;
+        } else {
+            labelText = "";
+            labelDescription = "";
         }
+
         final Resources res = mContext.getResources();
         if (mDual) {
-            mDualLabel = new QSDualTileLabel(mContext);
-            mDualLabel.setId(View.generateViewId());
-            mDualLabel.setBackgroundResource(R.drawable.btn_borderless_rect);
-            mDualLabel.setFirstLineCaret(mContext.getDrawable(R.drawable.qs_dual_tile_caret));
-            mDualLabel.setTextColor(mContext.getColor(R.color.qs_tile_text));
-            mDualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
-            mDualLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
-            mDualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+            if (mLabel != null) {
+                removeView(mLabel);
+                mLabel = null;
+            }
+
+            final QSDualTileLabel dualLabel = mDualLabel == null ?
+                    new QSDualTileLabel(mContext) : mDualLabel;
+            dualLabel.setId(View.generateViewId());
+            dualLabel.setBackgroundResource(R.drawable.btn_borderless_rect);
+            dualLabel.setFirstLineCaret(mContext.getDrawable(R.drawable.qs_dual_tile_caret));
+            dualLabel.setTextColor(mContext.getColor(R.color.qs_tile_text));
+            dualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
+            dualLabel.setTypeface(CONDENSED);
+            dualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
-            mDualLabel.setClickable(true);
-            mDualLabel.setOnClickListener(mClickSecondary);
-            mDualLabel.setFocusable(true);
-            if (labelText != null) {
-                mDualLabel.setText(labelText);
+            dualLabel.setClickable(true);
+            dualLabel.setOnClickListener(mClickSecondary);
+            dualLabel.setLongClickable(true);
+            dualLabel.setOnLongClickListener(mLongClick);
+            dualLabel.setFocusable(true);
+            dualLabel.setText(labelText);
+            dualLabel.setContentDescription(labelDescription);
+            if (mDualLabel == null) {
+                addView(mDualLabel = dualLabel);
             }
-            if (labelDescription != null) {
-                mDualLabel.setContentDescription(labelDescription);
-            }
-            addView(mDualLabel);
-            mDualLabel.setAccessibilityTraversalAfter(mTopBackgroundView.getId());
         } else {
-            mLabel = new TextView(mContext);
-            mLabel.setTextColor(mContext.getColor(R.color.qs_tile_text));
-            mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
-            mLabel.setMinLines(2);
-            mLabel.setPadding(0, 0, 0, 0);
-            mLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
-            mLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
-            mLabel.setClickable(false);
-            if (labelText != null) {
-                mLabel.setText(labelText);
+            if (mDualLabel != null) {
+                mDualLabel.setOnClickListener(null);
+                mDualLabel.setClickable(false);
+                mDualLabel.setOnLongClickListener(null);
+                mDualLabel.setLongClickable(false);
+                mDualLabel.setFocusable(false);
+
+                removeView(mDualLabel);
+                mDualLabel = null;
             }
-            addView(mLabel);
+
+            final TextView label = mLabel == null ? new TextView(mContext) : mLabel;
+            label.setId(android.R.id.title);
+            label.setTextColor(mContext.getColor(R.color.qs_tile_text));
+            label.setGravity(Gravity.CENTER_HORIZONTAL);
+            label.setMinLines(2);
+            label.setPadding(0, 0, 0, 0);
+            label.setTypeface(CONDENSED);
+            label.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                    res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
+            label.setClickable(false);
+            label.setText(labelText);
+            label.setContentDescription(labelDescription);
+            if (mLabel == null) {
+                addView(mLabel = label);
+            }
         }
     }
 
-    public boolean setDual(boolean dual) {
+    public boolean isDual() {
+        return mDual;
+    }
+
+    public synchronized void setDual(final boolean dual) {
         final boolean changed = dual != mDual;
         mDual = dual;
-        if (changed) {
-            recreateLabel();
-        }
+
         if (mTileBackground instanceof RippleDrawable) {
             setRipple((RippleDrawable) mTileBackground);
         }
+
+        final View priority = dual ? mTopBackgroundView : this;
+        final View other = dual ? this : mTopBackgroundView;
+
+        priority.setOnClickListener(mClickPrimary);
+        priority.setClickable(true);
+        priority.setOnLongClickListener(mLongClick);
+        priority.setLongClickable(true);
+        other.setOnClickListener(null);
+        other.setClickable(false);
+        other.setOnLongClickListener(null);
+        other.setLongClickable(false);
+
+        setImportantForAccessibility(dual ? View.IMPORTANT_FOR_ACCESSIBILITY_NO :
+                View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+
         if (dual) {
-            mTopBackgroundView.setOnClickListener(mClickPrimary);
-            mTopBackgroundView.setOnLongClickListener(mClickLong);
-            setOnClickListener(null);
-            setClickable(false);
-            setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
             mTopBackgroundView.setBackground(mTileBackground);
         } else {
-            mTopBackgroundView.setOnClickListener(null);
-            mTopBackgroundView.setClickable(false);
-            setOnClickListener(mClickPrimary);
-            setOnLongClickListener(mClickLong);
-            setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
             setBackground(mTileBackground);
         }
+
         mTopBackgroundView.setFocusable(dual);
         setFocusable(!dual);
         mDivider.setVisibility(dual ? VISIBLE : GONE);
+        if (changed) {
+            recreateLabel();
+            updateTopPadding();
+        }
         postInvalidate();
-        return changed;
     }
 
     private void setRipple(RippleDrawable tileBackground) {
@@ -210,10 +243,12 @@ public class QSTileView extends ViewGroup {
         }
     }
 
-    public void init(OnClickListener clickPrimary, OnClickListener clickSecondary, OnLongClickListener clickLong) {
+    public void init(OnClickListener clickPrimary, OnClickListener clickSecondary,
+            OnLongClickListener longClick, OnDragListener drag) {
         mClickPrimary = clickPrimary;
         mClickSecondary = clickSecondary;
-        mClickLong = clickLong;
+        mLongClick = longClick;
+        setOnDragListener(drag);
     }
 
     protected View createIcon() {
@@ -345,6 +380,10 @@ public class QSTileView extends ViewGroup {
         }
         firstView.setAccessibilityTraversalAfter(previousView.getId());
         return lastView;
+    }
+
+    public View.DragShadowBuilder getDragShadowBuilder() {
+        return new View.DragShadowBuilder(mIcon);
     }
 
     private class H extends Handler {

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -948,16 +948,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mUserSwitcherController, mKeyguardMonitor,
                     mSecurityController);
             mQSPanel.setHost(qsh);
-            mQSPanel.setTiles(qsh.getTiles());
-            mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindow);
-            mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+            mQSPanel.setBrightnessMirror(mBrightnessMirrorController =
+                    new BrightnessMirrorController(mStatusBarWindow));
             mHeader.setQSPanel(mQSPanel);
-            qsh.setCallback(new QSTileHost.Callback() {
-                @Override
-                public void onTilesChanged() {
-                    mQSPanel.setTiles(qsh.getTiles());
-                }
-            });
         }
 
         // User info. Trigger first load.