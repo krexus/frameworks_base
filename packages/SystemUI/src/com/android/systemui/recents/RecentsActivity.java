36bb20797578de0de5e96977f2a1d0a9936b29ce
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2016-11-21 15:42:20 +0900

    Don't include front most excluded task in Multi-Window mode
    RecentsActivity should not reload the tasks which include
    a front most excluded task in Multi-Window mode.
    
    Bug: 33031985
    Test: manual - display app task excluded from Overview screen and then
                   change font size in Multi-Window mode
    
    Change-Id: Ic0c35cd06c14d9ed8d2dc46a01f2153738bad849

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -396,7 +396,7 @@ public class RecentsActivity extends Activity implements ViewTreeObserver.OnPreD
         RecentsActivityLaunchState launchState = config.getLaunchState();
         if (!loadPlan.hasTasks()) {
             loader.preloadTasks(loadPlan, launchState.launchedToTaskId,
-                    !launchState.launchedFromHome);
+                    !launchState.launchedFromHome && !isInMultiWindowMode());
         }
 
         RecentsTaskLoadPlan.Options loadOpts = new RecentsTaskLoadPlan.Options();
