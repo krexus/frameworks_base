da0ed6db42cd9ab1d1ff16948d522f3fa6469c62
George G <kreach3r@users.noreply.github.com>
2016-12-31 23:12:50 +0200

    Revert "Disable power menu on secured lockscreen"
    This reverts commit 7ad257ce0b1f8cf6cde1c9266cb0cada93174c3e.
    
    Change-Id: Ie0128b82a6e50e3ab13a641dd080b3963d298e90

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1261,7 +1261,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private void powerLongPress() {
-        int behavior = getResolvedLongPressOnPowerBehavior();
+        final int behavior = getResolvedLongPressOnPowerBehavior();
         switch (behavior) {
         case LONG_PRESS_POWER_NOTHING:
             break;
@@ -1270,13 +1270,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false)) {
                 performAuditoryFeedbackForAccessibilityIfNeed();
             }
-	    boolean locked = isStatusBarKeyguard() && isKeyguardSecure(mCurrentUserId);
-            boolean globalActionsOnLockScreen = mContext.getResources().getBoolean(com.android.internal.R.bool.config_powerButtonLockscreenEnabled);
-            if (locked && !globalActionsOnLockScreen) {
-                behavior = LONG_PRESS_POWER_NOTHING;
-            } else {
-                showGlobalActionsInternal();
-            }
+            showGlobalActionsInternal();
             break;
         case LONG_PRESS_POWER_SHUT_OFF:
         case LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM:
