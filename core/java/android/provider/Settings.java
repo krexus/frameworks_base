c9d0af0dad825b30012a589edbc04053d8a7eb2a
George G <kreach3r@users.noreply.github.com>
2017-02-25 17:32:18 +0200

    base: Force authorize all calling packages by Masquerade (2/3)
    Change-Id: Iaf354c4d891acf9f5e19767223e4f9860a07338f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6566,6 +6566,12 @@ public final class Settings {
          */
         public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
 
+        /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by Masquerade
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
 
         /**
          * Status bar battery %
